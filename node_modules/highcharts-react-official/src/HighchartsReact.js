881083b48f4c47b96e30154a001ccf100154172d
witusj <wfth69@gmail.com>
2018-09-02 18:40:39 +0200

    Update

---
+++ node_modules/highcharts-react-official/src/HighchartsReact.js
@@ -0,0 +1,44 @@
+var React = require('react')
+var createReactClass = require('create-react-class')
+
+var HighchartsReact = createReactClass({
+  componentDidMount: function () {
+    var p = this.props
+    var highcharts = p.highcharts || window.Highcharts
+    var constructorType = p.constructorType || 'chart'
+    // Create chart
+    this.chart = highcharts[constructorType](this.container, Object.assign({}, p.options))
+  },
+
+  shouldComponentUpdate: function (nextProps, nextState) {
+    var update = this.props.update
+    // Update if not specified or set to true
+    return (typeof update === 'undefined') || update
+  },
+
+  componentDidUpdate: function () {
+    this.chart.update(Object.assign({}, this.props.options), true, !(this.props.oneToOne === false))
+  },
+
+  componentWillReceiveProps: function () {
+    this.chart.update(Object.assign({}, this.props.options), true, !(this.props.oneToOne === false))
+  },
+
+  componentWillUnmount: function () {
+    // Destroy chart
+    this.chart.destroy()
+  },
+
+  render: function () {
+    var self = this
+    var containerProps = this.props.containerProps || {}
+
+    // Add ref to div props
+    containerProps.ref = function (container) { self.container = container }
+
+    // Create container for our chart
+    return React.createElement('div', containerProps)
+  }
+})
+
+module.exports = HighchartsReact
